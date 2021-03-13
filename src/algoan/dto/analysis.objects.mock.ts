d455f6e06a83b61262aab80b2fe42b01785768be
Edouard Bougon <edouard@bougon.fr>
2021-03-13 19:17:54 +0100

    feat(algoan) Add mocks for dto

---
+++ src/algoan/dto/analysis.objects.mock.ts
@@ -0,0 +1,10 @@
+import { AnalysisStatus } from "./analysis.enum";
+import { Analysis } from "./analysis.objects";
+
+/**
+ * Analysis
+ */
+export const analysisMock: Analysis = {
+  id: 'Analysis-id',
+  status: AnalysisStatus.COMPLETED,
+}
---
+++ src/algoan/dto/customer.objects.mock.ts
@@ -0,0 +1,60 @@
+import { AggregationDetailsMode, AggregationDetailsAggregatorName } from "./customer.enums";
+import { AggregationDetails, PersonalDetails } from "./customer.objects";
+
+
+/**
+ * Mock for `AggregationDetails` object
+ */
+ export const aggregationDetailsMock: AggregationDetails = {
+  aggregatorName: AggregationDetailsAggregatorName.tink,
+  callbackUrl: 'callbackUrl',
+  token: 'token',
+  mode: AggregationDetailsMode.redirect,
+  redirectUrl: 'redirectUrl',
+  apiUrl: 'apiUrl',
+  userId: undefined,
+  clientId: 'clientId',
+}
+
+/**
+ * Mock for `PersonalDetailsIdentity` object
+ */
+export const personalDetailsIdentityMock = {
+  firstName: 'firstName',
+  lastName: 'lastName',
+  birthName: 'birthName',
+  birthDate: 'birthDate',
+  birthCity: 'birthCity',
+  birthZipCode: 'birthZipCode',
+  birthCountry: 'birthCountry',
+}
+
+/**
+ * Mock for `PersonalDetailsContact` object
+ */
+export const personalDetailsContactMock = {
+  email: 'email',
+  phoneNumber: 'phoneNumber',
+  street: 'street',
+  city: 'city',
+  zipCode: 'zipCode',
+  country: 'country',
+}
+
+/**
+ * Mock for `PersonalDetails` object
+ */
+export const personalDetailsMock: PersonalDetails = {
+  identity: personalDetailsIdentityMock,
+  contact: personalDetailsContactMock,
+}
+
+/**
+ * Mock for `Customer` object
+ */
+export const customerMock = {
+  id: '5f58a224aa55a30021bc7ab4',
+  customIdentifier: 'customIdentifier',
+  aggregationDetails: aggregationDetailsMock,
+  personalDetails: personalDetailsMock,
+}
