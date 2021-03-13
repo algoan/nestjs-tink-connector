c29ed7b0456cb066ff9b8f7babd3f4d6b98384b7
Edouard Bougon <edouard@bougon.fr>
2021-03-13 19:21:34 +0100

    feat(config) Add module to inject config

---
+++ src/config/config.module.ts
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { Module } from '@nestjs/common';
+import { config } from "node-config-ts";
+
+export const CONFIG: string = 'CONFIG';
+
+/**
+ * Config module
+ */
+@Module({
+  providers: [
+    {
+      provide: CONFIG,
+      useValue: config,
+    },
+  ],
+  exports: [
+    CONFIG,
+  ]
+})
+export class ConfigModule {}
