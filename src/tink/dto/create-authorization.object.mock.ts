368974a4ae2a55071ef9bfb8d9af130844a4cf90
Edouard Bougon <edouard@bougon.fr>
2021-03-13 19:24:11 +0100

    feat(tink) Add mocks for dto

---
+++ src/tink/dto/create-authorization.object.mock.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/naming-convention,camelcase */
+
+import { CreateAuthorizationObject } from "./create-authorization.object";
+
+/**
+ * Mock of a new authorization code
+ */
+ export const createAuthorizationObjectMock: CreateAuthorizationObject = {
+  code: 'c50cd6960a6f44ffb701ef60fafa7761',
+}
---
+++ src/tink/dto/create-user.object.mock.ts
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/naming-convention,camelcase */
+
+import { CreateUserObject } from "./create-user.object";
+
+/**
+ * Mock of a new user
+ */
+export const createUserObject: CreateUserObject = {
+  external_user_id: 'external_user_id',
+  user_id: 'user_id',
+}
